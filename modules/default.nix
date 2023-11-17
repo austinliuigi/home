19b8bda48db6a6134010ad9782350b1fe28f8e7b
Austin Liu <austinhliu@gmail.com>
2023-11-16 22:16:46 -0800

    feat: allow helper files in modules
    - files in `modules/` that are prefixed with and underscore won't get
      imported as modules

--- modules/default.nix
+++ modules/default.nix
@@ -1,9 +1,14 @@
 { config, pkgs, lib, utils, ... }:
 
 {
-  # import all modules; remove current file from list of files to avoid circular import
+  # import all modules
+  # - remove current file from list of files to avoid circular import
+  # - don't include files with a starting underscore
   imports = (lib.remove
     "${./.}/default.nix"
-    (utils.scanDirFiles { dir = ./.; absolute_paths = true; })
+    (builtins.filter
+      (elem: (builtins.match "_.*" (builtins.baseNameOf elem)) == null)
+      (utils.scanDirFiles { dir = ./.; absolute_paths = true; })
+    )
   );
 }
