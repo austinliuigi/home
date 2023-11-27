3ad1c9642ae2697ad14bdaccb4a7ee65a50b411d
Austin Liu <austinhliu@gmail.com>
2023-11-27 12:55:10 -0800

    refactor(hyprland): symlink native config files

--- modules/programs/wayland/hyprland/default.nix
+++ modules/programs/wayland/hyprland/default.nix
@@ -7,20 +7,20 @@ in
   options.modules.programs.hyprland.enable = lib.mkEnableOption "hyprland module";
 
   config = lib.mkIf cfg.enable {
-    # xdg.configFile = utils.interpolateConfig "${/. + "${config.dotfiles}/hyprland"}";
-
     # Manage configuration for hyprland
     # - this adds systemd support -> graphical-session.target gets run
     #     - this is needed for some gui systemd reliant programs, e.g. kdeconnect
     wayland.windowManager.hyprland = {
       enable = true;
       extraConfig = ''
-      source = ${config.dotfiles.hyprland}/.config/hypr/hyprland/settings.conf
-      source = ${config.dotfiles.hyprland}/.config/hypr/hyprland/keybinds.conf
-      source = ${config.dotfiles.hyprland}/.config/hypr/hyprland/rules.conf
+      source = ${config.home.homeDirectory}/.config/hypr/hyprland/settings.conf
+      source = ${config.home.homeDirectory}/.config/hypr/hyprland/keybinds.conf
+      source = ${config.home.homeDirectory}/.config/hypr/hyprland/rules.conf
       exec-once = waybar & hyprpaper &
       '';
     };
+
+    xdg.configFile."hypr/hyprland".source = config.lib.file.mkOutOfStoreSymlink "${config.dotfiles.hyprland}/.config/hypr/hyprland";
 
     # Add scripts that config depends on
     home.packages = [
