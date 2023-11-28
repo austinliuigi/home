2f58c6a789b1879a119e14149cb31ef4bee826e6
Austin Liu <austinhliu@gmail.com>
2023-11-27 19:56:35 -0800

    feat: allow interpolating transparency along with colors

--- modules/colors.nix
+++ modules/colors.nix
@@ -6,11 +6,22 @@
   ];
 
   config = {
-    colorscheme = inputs.nix-colors.colorschemes.gruvbox-dark-medium;
+    # https://github.com/tinted-theming/base16-schemes
+    colorscheme = inputs.nix-colors.colorschemes.nord;
 
     # config.colorscheme defined at https://github.com/Misterio77/nix-colors/blob/main/module/colorscheme.nix
-    configuration = {
-      substitutions = config.colorscheme.colors;
+    configuration =
+    let
+      transparent = "false";
+      opacity_ratio = if transparent == "true" then "0.0" else "1.0";
+      opacity_percentage = if transparent == "true" then "0" else "100";
+    in
+    {
+      substitutions = config.colorscheme.colors // {
+        transparent = transparent;
+        opacity_ratio = opacity_ratio;
+        opacity_percentage = opacity_percentage;
+      };
     };
   };
 }
