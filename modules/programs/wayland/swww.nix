abfcb95f8d4efaacee112763d70197b62ba98100
Austin Liu <austinhliu@gmail.com>
2023-11-29 15:30:11 -0800

    refactor(swww): use dummy file instead of home.activation to automatically alter wallpaper on colorscheme change

--- modules/programs/wayland/swww.nix
+++ modules/programs/wayland/swww.nix
@@ -9,12 +9,25 @@ in
   config = lib.mkIf cfg.enable {
     home.packages = [ pkgs.swww ];
 
-    home.activation = {
-      wallpaper = lib.hm.dag.entryAfter ["writeBoundary"] ''
-        if [ ! -d ~/wallpapers ]; then $DRY_RUN_CMD mkdir $VERBOSE_ARG ~/wallpapers; fi
-        $DRY_RUN_CMD ${pkgs.imagemagick}/bin/convert ${config.dotfiles._wallpapers}/wallpaper.png -fill \#${config.colorscheme.colors.base02} -tint 50 ~/wallpapers/wallpaper.png 2> /dev/null
-        ${pkgs.swww}/bin/swww img ~/wallpapers/wallpaper.png
-      '';
+    home.file = {
+      "wallpapers/palette.dummy" = {
+        text = ''
+          # this is a dummy file used to detect a colorscheme change in nix
+          ${config.colorscheme.name}
+        '';
+        onChange = ''
+          ${pkgs.imagemagick}/bin/convert ${config.dotfiles._wallpapers}/wallpaper.png -fill \#${config.colorscheme.colors.base02} -tint 100 ~/wallpapers/wallpaper.png 2> /dev/null
+
+          ${pkgs.swww}/bin/swww img ~/wallpapers/wallpaper.png
+        '';
+      };
     };
+    # home.activation = {
+    #   wallpaper = lib.hm.dag.entryAfter ["writeBoundary"] ''
+    #     if [ ! -d ~/wallpapers ]; then $DRY_RUN_CMD mkdir $VERBOSE_ARG ~/wallpapers; fi
+    #     $DRY_RUN_CMD ${pkgs.imagemagick}/bin/convert ${config.dotfiles._wallpapers}/wallpaper.png -fill \#${config.colorscheme.colors.base02} -tint 100 ~/wallpapers/wallpaper.png 2> /dev/null
+    #     ${pkgs.swww}/bin/swww img ~/wallpapers/wallpaper.png
+    #   '';
+    # };
   };
 }
