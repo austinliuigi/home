725b68b7d2059a48d357db27addae1040de30712
Austin Liu <austinhliu@gmail.com>
2023-11-29 15:28:35 -0800

    feat: update waybar automatically upon colorscheme change

--- modules/programs/wayland/waybar.nix
+++ modules/programs/wayland/waybar.nix
@@ -8,5 +8,19 @@ in
 
   config = lib.mkIf cfg.enable {
     home.packages = [ pkgs.waybar ];
+
+    home.file = {
+      ".local/share/waybar/palette.css" = {
+        text = config.configuration.interpolateConfigFileWithMsg { file = "${/. + "${config.dotfiles.waybar}/.local/share/waybar/palette.css"}"; comment_start = "/*"; comment_end = "*/"; };
+        onChange = ''
+          procs=$(${pkgs.busybox}/bin/pgrep waybar)
+          if [ -n "$procs" ]; then kill $procs; fi
+
+          PATH=${pkgs.hyprland}/bin:$PATH # wayland needs hyprctl for hyprland ipc
+          ${pkgs.waybar}/bin/waybar &
+        '';
+      };
+      ".config/waybar".source = config.lib.file.mkOutOfStoreSymlink "${config.dotfiles.waybar}/.config/waybar";
+    };
   };
 }
