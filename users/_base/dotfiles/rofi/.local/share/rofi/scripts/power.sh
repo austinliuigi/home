9c58629b6d71a6d256d8361bd0aa354b84d3dd58
Austin Liu <austinhliu@gmail.com>
2023-12-05 21:12:48 -0800

    feat(rofi): add suspend option to power modi

--- users/_base/dotfiles/rofi/.local/share/rofi/scripts/power.sh
+++ users/_base/dotfiles/rofi/.local/share/rofi/scripts/power.sh
@@ -5,6 +5,7 @@ then
     # list out choices
     echo "poweroff"
     echo "restart"
+    echo "suspend"
     echo "lock"
     exit 0
 fi
@@ -12,6 +13,7 @@ fi
 # handle selected choice
 case "$1" in
     "poweroff") poweroff ;;
-    "restart") reboot ;;
+    "reboot") reboot ;;
+    "suspend") systemctl suspend ;;
     "lock") loginctl lock-session ;;
 esac
