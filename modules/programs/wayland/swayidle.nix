4af694ef2ec402f6dfc92d95c99b58d808be66e1
Austin Liu <austinhliu@gmail.com>
2023-12-06 20:45:13 -0800

    feat(swayidle): use swaylock-effects

--- modules/programs/wayland/swayidle.nix
+++ modules/programs/wayland/swayidle.nix
@@ -9,7 +9,45 @@ in
   config = lib.mkIf cfg.enable {
     services.swayidle =
       let 
-        lock_cmd = "${pkgs.swaylock}/bin/swaylock -fF --color 0000ff --ring-color aaaaaa --key-hl-color dddddd -l";
+        # lock_cmd = "${pkgs.swaylock-effects}/bin/swaylock -fF --color 0000ff --ring-color aaaaaa --key-hl-color dddddd -l";
+        lock_cmd = lib.strings.concatStringsSep " " [
+          "${pkgs.swaylock-effects}/bin/swaylock"
+          "--screenshots"
+          "--effect-blur 7x5"
+          # "--fade-in 0.5"
+          "--font 'Hack Nerd Font'"
+          "--clock"
+          "--indicator"
+
+          "--key-hl-color ${config.colorscheme.colors.base0D}"   # color of line that shows on keypress
+          "--separator-color 00000000"                           # color of border of line that shows on keypress
+          "--bs-hl-color ${config.colorscheme.colors.base08}"    # color of line that shows on backspace
+
+          "--text-color ${config.colorscheme.colors.base0D}"     # color of text
+          "--ring-color 00000000"                                # color of ring
+          "--line-color ${config.colorscheme.colors.base0D}"     # color of ring border
+          "--inside-color ${config.colorscheme.colors.base02}aa" # color of fill circle
+
+          "--text-ver-color ${config.colorscheme.colors.base0C}"
+          "--ring-ver-color 00000000"
+          "--line-ver-color ${config.colorscheme.colors.base0C}"
+          "--inside-ver-color ${config.colorscheme.colors.base02}aa"
+
+          "--text-wrong-color ${config.colorscheme.colors.base08}"
+          "--ring-wrong-color 00000000"
+          "--line-wrong-color ${config.colorscheme.colors.base08}"
+          "--inside-wrong-color ${config.colorscheme.colors.base02}aa"
+
+          "--text-clear-color ${config.colorscheme.colors.base0A}"
+          "--ring-clear-color 00000000"
+          "--line-clear-color ${config.colorscheme.colors.base0A}"
+          "--inside-clear-color ${config.colorscheme.colors.base02}aa"
+
+          "--text-caps-lock-color ${config.colorscheme.colors.base09}"
+          "--ring-caps-lock-color 00000000"
+          "--line-caps-lock-color ${config.colorscheme.colors.base09}"
+          "--inside-caps-lock-color ${config.colorscheme.colors.base02}aa"
+        ];
         dpms_cmd = "${pkgs.hyprland}/bin/hyprctl dispatch dpms";
       in {
         enable = true;
