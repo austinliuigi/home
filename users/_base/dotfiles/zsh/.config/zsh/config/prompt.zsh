bf12a2152162deac856b0eae915f597257f94058
Austin Liu <austinhliu@gmail.com>
2025-01-30 11:16:52 -0800

    refactor(zsh): clean up prompt script

--- users/_base/dotfiles/zsh/.config/zsh/config/prompt.zsh
+++ users/_base/dotfiles/zsh/.config/zsh/config/prompt.zsh
@@ -1,3 +1,5 @@
+# ========== UTILS ==========
+
 # Calculate how many characters a string takes up in prompt (accounts
 # for escaping & wide characters)
 function promptLength () {
@@ -10,12 +12,12 @@ function promptLength () {
     # If u is non-zero
     if (( u )); then
         # http://zsh.zle.lc/zsh-conditional-substrings-prompts.html
-	# https://zsh.sourceforge.io/Doc/Release/Expansion.html (look at section 14.3.1 for explanation of ${(%):-""})
-	# While length of $1 (after escaping) is less than u
+        # https://zsh.sourceforge.io/Doc/Release/Expansion.html (look at section 14.3.1 for explanation of ${(%):-""})
+        # While length of $1 (after escaping) is less than u
         # Note: there is nothing before :- therefore what comes after it is ALWAYS substituted
         # Note: $1%$u(l.1.0) appends 1 if length of $1 is greater than u, else 0
-	# Note: the [-1] index gets the last character of the string (the 1/0 that was appended)
-	# After this while loop, l is smaller than length of $1 (after escaping), and u is larger
+        # Note: the [-1] index gets the last character of the string (the 1/0 that was appended)
+        # After this while loop, l is smaller than length of $1 (after escaping), and u is larger
         while (( ${${(%):-$1%$u(l.1.0)}[-1]} )); do
             l=u
             (( u *= 2 ))
@@ -57,6 +59,22 @@ function padMiddle () {
     fi
 }
 
+
+
+# ========== PROMPT SECTIONS ==========
+
+# Prompt styles
+PS_DEFAULT='%b%F{white}'
+PS_ROOT='%B%F{196}'
+PS_USR_HST='%B%F{blue}'
+PS_CWD='%B%F{cyan}'
+PS_GIT='%B%F{magenta}'
+
+# Static prompts (doesn't require manually updating)
+USER_HOSTNAME_PROMPT="${PS_USR_HST}(%(!.${PS_ROOT}.)%n%(!.${PS_USR_HST}.) @ %m)${PS_DEFAULT}"
+CWD_PROMPT="${PS_CWD}[%~]${PS_DEFAULT}"
+
+# Dynamic prompts (requires calling function to update)
 function sshPromptUpdate () {
     # Use default zsh options
     emulate -L zsh
@@ -66,7 +84,7 @@ function sshPromptUpdate () {
 
     # Set prompt for ssh sessions
     if [[ "${SSH_TTY}" ]]; then
-	    SSH_PROMPT="(ssh)-"
+        SSH_PROMPT="(ssh)-"
     fi
 }
 
@@ -76,28 +94,22 @@ function venvPromptUpdate () {
 
     # Initialize venv prompt
     typeset -g VENV_PROMPT=''
+    local in_venv="false"
 
-    # Set prompt for conda
+    # Conda virtual environments
     if [[ "${CONDA_DEFAULT_ENV}" ]]; then
-	    VENV_PROMPT="(${CONDA_DEFAULT_ENV})-"
+        VENV_PATH="${CONDA_DEFAULT_ENV}-"
+        in_venv="true"
     fi
 
-    # Set prompt for virtual environments
+    # General virtual environments
     if [[ "${VIRTUAL_ENV}" ]]; then
-	    VENV_PROMPT="(${VIRTUAL_ENV})-"
+        VENV_PATH="${VIRTUAL_ENV}"
+        in_venv="true"
     fi
-}
 
-function abducoPromptUpdate () {
-    # Use default zsh options
-    emulate -L zsh
-
-    # Initialize venv prompt
-    typeset -g ABDUCO_PROMPT=''
-
-    # Set prompt for virtual environments
-    if [[ "${ABDUCO_SESSION}" ]]; then
-	    ABDUCO_PROMPT=" %B${ABDUCO_SESSION}%b "
+    if [[ "${in_venv}" = "true" ]]; then
+      VENV_PROMPT=(venv .../"$(basename "$(dirname "$VENV_PATH")")"/"$(basename "$VENV_PATH")")
     fi
 }
 
@@ -116,14 +128,13 @@ function gitstatusPromptUpdate () {
     gitstatus_query 'GS'                  || return 1  # error
     [[ $VCS_STATUS_RESULT == 'ok-sync' ]] || return 0  # not a git repo
 
-    local g='%F{142}'   # green foreground
-    local y='%F{220}'   # yellow foreground
-    local b='%F{152}'   # blue foreground
-    local r='%F{174}'   # red foreground
+    local g='%F{142}'   # green
+    local y='%F{220}'   # yellow
+    local b='%F{152}'   # blue
+    local r='%F{167}'   # red
 
     # Initialize temporary git prompt holder
     local p
-    local branch_color="$PROMPT_COLOR_BRANCH"
 
     # Determine location of HEAD
     local where  # branch name, tag or commit
@@ -138,7 +149,7 @@ function gitstatusPromptUpdate () {
     fi
 
     (( $#where > 32 )) && where[13,-13]="…" # truncate long branch names and tags
-    p+="${branch_color}[${clean}${where//\%/%%}"     # escape %
+    p+="${PS_GIT}[${clean}${where//\%/%%}"     # escape %
 
     # $ if have stashes.
     (( VCS_STATUS_STASHES        )) && p+=" ${g}$"
@@ -163,37 +174,25 @@ function gitstatusPromptUpdate () {
     # 'merge' if the repo is in an unusual state.
     [[ -n $VCS_STATUS_ACTION     ]] && p+=" ${r}${VCS_STATUS_ACTION}"
 
-    GITSTATUS_PROMPT="-${p}${branch_color}]%f"
+    GITSTATUS_PROMPT="-${p}${PS_GIT}]${PS_DEFAULT}"
 }
 
-PROMPT_COLOR_USER='%F{blue}'
-PROMPT_COLOR_PATH='%F{cyan}'
-PROMPT_COLOR_BRANCH='%F{magenta}'
+
+
+# ========== PROMPT ==========
 
 function setPrompt () {
     # Use default zsh options
     emulate -L zsh
 
-    # Update ssh status
+    # Update dynamic prompts
     sshPromptUpdate
-
-    # Update venv status
     venvPromptUpdate
-
-    # Update abduco session status
-    abducoPromptUpdate
-
-    # Get git information
     gitstatusPromptUpdate
 
-    # Set prompt information
-    local default_color='%F{white}'
-    local user_color="$PROMPT_COLOR_USER"
-    local path_color="$PROMPT_COLOR_PATH"
-
-    local top_left=$'\n'"%f╭─${SSH_PROMPT}${VENV_PROMPT}${user_color}(%(!.%F{196}.)%n%(!.${user_color}.) @ %m)%f-${path_color}[%~]%f${GITSTATUS_PROMPT}%f"
-    local top_right="${ABDUCO_PROMPT}%f─╮"
-    local bottom_left="%f╰─ ᛋ "
+    local top_left=$'\n'"%f╭─${SSH_PROMPT}${USER_HOSTNAME_PROMPT}-${CWD_PROMPT}${GITSTATUS_PROMPT}"
+    local top_right="${VENV_PROMPT}─╮"
+    local bottom_left="╰─ ᛋ "
     local bottom_right="%F{green}󰟠%f ─╯"
 
     # Call padMiddle to set RETURN
@@ -204,43 +203,43 @@ function setPrompt () {
     RPROMPT=$bottom_right
 }
 
-# Set prompt shell options
-setopt no_prompt_{bang,subst} prompt_{cr,percent,sp}
-# Add hook function (call setPrompt before each new prompt)
-autoload -Uz add-zsh-hook
-add-zsh-hook precmd setPrompt
-
-function setTransientPrompt () {
-    local default_color='%F{white}'
-    local user_color="$PROMPT_COLOR_USER"
-    local path_color="$PROMPT_COLOR_PATH"
-    local branch_color="$PROMPT_COLOR_BRANCH"
-
-    # PROMPT=$'\n'"${default_color}» ${user_color}(%(!.%F{196}.)%n%(!.${user_color}.) @ %m)${default_color}-${path_color}[%~]${default_color} ᛋ "
-    PROMPT=$'\n'"%f╭─${SSH_PROMPT}${VENV_PROMPT}${user_color}(%(!.%F{196}.)%n%(!.${user_color}.) @ %m)%f-${path_color}[%~]${branch_color}${GITSTATUS_PROMPT}%f"$'\n'"%f╰─ ᛋ "
+function setPastPrompt () {
+    PROMPT=$'\n'"%f╭─${SSH_PROMPT}${VENV_PROMPT}${USER_HOSTNAME_PROMPT}-${CWD_PROMPT}${GITSTATUS_PROMPT}%f"$'\n'"%f╰─ ᛋ "
     RPROMPT=''
 }
 
-# Transient prompt (change current prompt before each new prompt)
+# Set prompt shell options
+setopt no_prompt_{bang,subst} prompt_{cr,percent,sp}
+zle_highlight=(default:bold) # set style for the typed out command itself
+
+# Current prompt (call setPrompt before each new prompt)
+autoload -Uz add-zsh-hook
+add-zsh-hook precmd setPrompt
+
+# Past prompt (change current prompt before each new prompt)
 zle-line-finish() {
-    setTransientPrompt
+    setPastPrompt
     zle reset-prompt
 }
 zle -N zle-line-finish
 
-# Transient prompt upon interrupt (change current prompt before abort signal)
+
+
+# ========== SIGNAL HANDLERS ==========
+
+# Change current prompt to past prompt upon interrupt (change current prompt before abort signal)
 # Programs terminated by uncaught signals typically return the status 128 plus the signal number
 # Hence the following causes the handler for SIGINT to change the prompt, then mimic the usual effect of the signal
 # https://zsh.sourceforge.io/Doc/Release/Functions.html#Trap-Functions
 TRAPINT () {
-	setTransientPrompt
-	zle reset-prompt 2> /dev/null
-	setPrompt
-	return $(( 128 + $1 ))
+    setPastPrompt
+    zle reset-prompt 2> /dev/null
+    setPrompt
+    return $(( 128 + $1 ))
 }
 
 # Recalculate current prompt after window resize
 TRAPWINCH () {
-	setPrompt
-	zle reset-prompt
+    setPrompt
+    zle reset-prompt
 }
