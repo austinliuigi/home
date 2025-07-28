938326020a869ccd2445c067e258c639a2c9e3ab
Austin Liu <austinhliu@gmail.com>
2025-07-27 17:30:47 -0700

    fix(btop): force palette.theme link to be created

--- modules/programs/btop.nix
+++ modules/programs/btop.nix
@@ -20,7 +20,7 @@ in
       # btop only correctly adds user themes in $XDG_CONFIG_DIR/btop/themes/; themes in other directories will not load
       # we link to the interpolated palette theme during activation so that the hoem directory is not hardcoded in the symlink
       btopLinkTheme = lib.hm.dag.entryAfter ["writeBoundary"] ''
-        run ln -s $VERBOSE_ARG "$HOME/.local/share/btop/palette.theme" "$HOME/.config/btop/themes/palette.theme" || true
+        run ln -f -s $VERBOSE_ARG "$HOME/.local/share/btop/palette.theme" "$HOME/.config/btop/themes/palette.theme" || true
       '';
     };
   };
