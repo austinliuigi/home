c0cd9f8551ef0a07d82bd6fc14043ae35e7bbe49
Austin Liu <austinhliu@gmail.com>
2025-08-01 12:46:28 -0700

    refactor(syncthing): reorganize module

--- modules/programs/syncthing.nix
+++ modules/programs/syncthing.nix
@@ -12,51 +12,87 @@
   cfg = config.modules.programs.syncthing;
   hub = "cloudlab"; # the server that acts as the central hub in my cluster
 
-  # NOTE: any syncthing device configuration parameters can be put here: https://docs.syncthing.net/v1.29.3/users/config.html#device-element
+  #===================================================================================================
+  # DEVICES
+  # - any [syncthing device configuration parameters](https://docs.syncthing.net/v1.29.3/users/config.html#device-element) can be put here
   #   - types are defined [here](https://github.com/syncthing/syncthing/blob/main/lib/config/deviceconfiguration.go)
   #   - you can also see an example json configuration by querying an existing syncthing instance, e.g. `curl -X GET -H "X-API-Key: <API_KEY>" http://localhost:8384/rest/config/devices/UNUYF5C-Q5K4AGX-FCBNG35-ET3W2YN-TYFDGVG-WQNGWDN-EOE66R3-WTOR7QH`
-  devices = {
+  #===================================================================================================
+  deviceBase = {
+    autoAcceptFolders = false;
+  };
+
+  devices = lib.mapAttrs (name: value: deviceBase // value) {
     cloudlab = {
       id = "ST7U3RW-RE6UF3O-72N57KP-C54CPWS-B2ZTCVP-TTM47LW-447V7IP-36SVBQC";
-      autoAcceptFolders = false;
-      introducer = true;
     };
     x1-carbon = {
       id = "UNUYF5C-Q5K4AGX-FCBNG35-ET3W2YN-TYFDGVG-WQNGWDN-EOE66R3-WTOR7QH";
-      autoAcceptFolders = false;
-      introducer = false;
     };
     m1-macbook-air = {
       id = "INADG3A-GATQUGJ-GHNIWTK-32LQDID-DGWB36R-CZJNZPS-JPJI36C-BSNJKQL";
-      autoAcceptFolders = false;
-      introducer = false;
     };
     ghost-s1 = {
       id = "2DWIXMI-ALEUPVJ-NR344RT-RIIBG6E-JLT4ABC-Y6JW23K-WWH3L67-VURIGAC";
-      autoAcceptFolders = false;
-      introducer = false;
     };
     phone = {
       id = "DSL3FCT-A44MBOJ-MJBRQDO-5Y2V2QY-NMS3YQJ-OIL5GYV-OBAXMB3-NE7OQAM";
-      autoAcceptFolders = false;
-      introducer = false;
     };
   };
 
-  # NOTE: any syncthing folder configuration parameters can be put here: https://docs.syncthing.net/v1.29.3/users/config.html#folder-element
+  #===================================================================================================
+  # FOLDERS
+  # - any [syncthing folder configuration parameters](https://docs.syncthing.net/v1.29.3/users/config.html#folder-element) can be put here
   #   - types are defined [here](https://github.com/syncthing/syncthing/blob/main/lib/config/folderconfiguration.go)
   #   - you can also see an example json configuration by querying an existing syncthing instance, e.g. `curl -X GET -H "X-API-Key: <API_KEY>" http://localhost:8384/rest/config/folders/pypev-zfwnm`
+  #===================================================================================================
   folderBase = {
+    enable = true;
     devices =
       if (hostname == hub)
       then builtins.attrNames (builtins.removeAttrs devices [hub])
       else [hub];
     rescanIntervalS = 1800;
-    # versioning = {
-    #   type = "";
-    #   params = {
-    #   };
-    # };
+  };
+
+  folders = lib.mapAttrs (name: value: folderBase // value) {
+    "backlog" = {
+      id = "dgzrv-myjtx";
+      path = "${config.home.homeDirectory}/backlog";
+    };
+    "cookbook" = {
+      id = "7tnly-dyrts";
+      path = "${config.home.homeDirectory}/cookbook";
+    };
+    "checklists" = {
+      id = "jnvdc-tncl6";
+      path = "${config.home.homeDirectory}/checklists";
+    };
+    "todo" = {
+      id = "mwxtf-jm7w5";
+      path = "${config.home.homeDirectory}/todo";
+    };
+    "guides" = {
+      id = "rpect-q9ay6";
+      path = "${config.home.homeDirectory}/guides";
+    };
+    "jobs" = {
+      id = "bbnen-rjqm5";
+      path = "${config.home.homeDirectory}/jobs";
+    };
+    "notes" = {
+      id = "pypev-zfwnm";
+      path = "${config.home.homeDirectory}/notes";
+    };
+    "scratchpad" = {
+      id = "s4ykd-trtwt";
+      path = "${config.home.homeDirectory}/scratchpad";
+    };
+    "textbooks" = {
+      enable = false;
+      id = "477wq-kdsru";
+      path = "${config.home.homeDirectory}/textbooks";
+    };
   };
 in {
   options.modules.programs.syncthing = {
@@ -71,72 +107,8 @@ in {
         devices =
           if (hostname == hub)
           then builtins.removeAttrs devices [hub]
-          else builtins.removeAttrs devices (lib.lists.remove hub (builtins.attrNames devices));
-        folders = {
-          "backlog" =
-            {
-              enable = true;
-              id = "dgzrv-myjtx";
-              path = "${config.home.homeDirectory}/backlog";
-            }
-            // folderBase;
-          "cookbook" =
-            {
-              enable = true;
-              id = "7tnly-dyrts";
-              path = "${config.home.homeDirectory}/cookbook";
-            }
-            // folderBase;
-          "checklists" =
-            {
-              enable = true;
-              id = "jnvdc-tncl6";
-              path = "${config.home.homeDirectory}/checklists";
-            }
-            // folderBase;
-          "todo" =
-            {
-              enable = true;
-              id = "mwxtf-jm7w5";
-              path = "${config.home.homeDirectory}/todo";
-            }
-            // folderBase;
-          "guides" =
-            {
-              enable = true;
-              id = "rpect-q9ay6";
-              path = "${config.home.homeDirectory}/guides";
-            }
-            // folderBase;
-          "jobs" =
-            {
-              enable = true;
-              id = "bbnen-rjqm5";
-              path = "${config.home.homeDirectory}/jobs";
-            }
-            // folderBase;
-          "notes" =
-            {
-              enable = true;
-              id = "pypev-zfwnm";
-              path = "${config.home.homeDirectory}/notes";
-            }
-            // folderBase;
-          "scratchpad" =
-            {
-              enable = true;
-              id = "s4ykd-trtwt";
-              path = "${config.home.homeDirectory}/scratchpad";
-            }
-            // folderBase;
-          "textbooks" =
-            {
-              enable = false;
-              id = "477wq-kdsru";
-              path = "${config.home.homeDirectory}/textbooks";
-            }
-            // folderBase;
-        };
+          else {${hub} = devices.${hub} // {introducer = true;};};
+        folders = folders;
       };
     };
   };
