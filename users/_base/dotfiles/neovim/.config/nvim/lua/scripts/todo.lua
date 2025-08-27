32180a3eea362b6401a03e5ac03499ac285687f4
Austin Liu <austinhliu@gmail.com>
2025-08-27 15:45:04 -0700

    feat(neovim): add :TodoReschedule

--- users/_base/dotfiles/neovim/.config/nvim/lua/scripts/todo.lua
+++ users/_base/dotfiles/neovim/.config/nvim/lua/scripts/todo.lua
@@ -4,21 +4,51 @@ vim.g.todo_ignore_invalid_args = true
 local todo_dir = vim.fn.expand("~") .. "/todo"
 local calendar_dir = todo_dir .. "/calendar"
 
+--====================================================================================================
+-- Types
+--====================================================================================================
+
+---@alias todo.Date string Format: yyyy/mm/dd (e.g. 2020/03/14)
+
+---@alias todo.TodaySpec "today"
+---@alias todo.AbsoluteDaySpec string Format: <N>d (e.g. 21d)
+---@alias todo.AbsoluteMonthSpec string Format: <N>m (e.g. 8m)
+---@alias todo.AbsoluteYearSpec string Format: <N>y (e.g. 2020y)
+---@alias todo.RelativeDaySpec string Format: (+|-)<N>d (e.g. +21d)
+---@alias todo.RelativeWeekSpec string Format: (+|-)<N>w (e.g. +5w)
+---@alias todo.RelativeYearSpec string Format: (+|-)<N>y (e.g. +2y)
+---@alias todo.WeekdayNameSpec string Format: "mon[day]"|"tue[sday]"|"wed[nedday]"|"thu[rsday]"|"fri[day]"
+
+---@alias todo.Spec todo.TodaySpec|todo.AbsoluteDaySpec|todo.AbsoluteMonthSpec|todo.AbsoluteYearSpec|todo.RelativeDaySpec|todo.RelativeWeekSpec|todo.RelativeYearSpec|todo.WeekdayNameSpec
+
+--====================================================================================================
+-- Utils
+--====================================================================================================
+
+---@param date todo.Date
+---@return integer time
 local function date_to_time(date)
   local _, _, year, month, day = date:find("(%d%d%d%d)/(%d%d)/(%d%d)")
   return os.time({ year = year, month = month, day = day })
 end
 
--- If current buffer is in calendar, start relative to that day, otherwise start relative to current day
+--- If current buffer is in calendar, start relative to that day, otherwise start relative to current day
+---
+---@return integer start_time
 local function get_start_time()
   local _, _, buf_date = string.find(vim.api.nvim_buf_get_name(0), calendar_dir .. "/(%d%d%d%d/%d%d/%d%d).norg")
   return (buf_date == nil) and os.time() or date_to_time(buf_date)
 end
 
+---@param year string|integer
+---@return boolean
 local function is_leap_year(year)
   return year % 4 == 0 and (year % 100 ~= 0 or year % 400 == 0)
 end
 
+---@param month integer 1-12
+---@param year integer
+---@return integer days_in_month
 local function get_days_in_month(month, year)
   local days_in_month = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 }
   if month == 2 and is_leap_year(year) then
@@ -28,6 +58,10 @@ local function get_days_in_month(month, year)
   end
 end
 
+--- Check if date is valid (exists in the calendar)
+---
+---@param date todo.Date
+---@return boolean
 local function is_valid_date(date)
   local _, _, year, month, day = date:find("(%d%d%d%d)/(%d%d)/(%d%d)")
   year = tonumber(year)
@@ -37,6 +71,7 @@ local function is_valid_date(date)
   if month < 1 or month > 12 then
     return false
   end
+  ---@diagnostic disable-next-line: param-type-mismatch
   if day < 1 or day > get_days_in_month(month, year) then
     return false
   end
@@ -50,6 +85,7 @@ end
 ------------------------------------------------------------------------------------------------------
 -- Today
 ------------------------------------------------------------------------------------------------------
+
 ---@param spec string -- Expected format: "today"
 ---@return number? computed_time
 local function handle_today(spec)
@@ -73,6 +109,8 @@ local function handle_absolute_day(spec, start_time)
     return nil
   end
 
+  ---@type string
+  ---@diagnostic disable-next-line: assign-type-mismatch
   local computed_date = os.date("%Y/%m/" .. string.format("%02d", day), start_time or get_start_time())
   if not is_valid_date(computed_date) and vim.g.todo_strict_dates then
     return nil
@@ -89,6 +127,8 @@ local function handle_absolute_month(spec, start_time)
     return nil
   end
 
+  ---@type string
+  ---@diagnostic disable-next-line: assign-type-mismatch
   local computed_date = os.date("%Y/" .. string.format("%02d", month) .. "/%d", start_time or get_start_time())
   if not is_valid_date(computed_date) and vim.g.todo_strict_dates then
     return nil
@@ -105,6 +145,8 @@ local function handle_absolute_year(spec, start_time)
     return nil
   end
 
+  ---@type string
+  ---@diagnostic disable-next-line: assign-type-mismatch
   local computed_date = os.date(string.format("%04d", year) .. "/%m/%d", start_time or get_start_time())
   if not is_valid_date(computed_date) and vim.g.todo_strict_dates then
     return nil
@@ -154,9 +196,12 @@ local function handle_relative_year(spec, start_time)
   end
 
   -- account for leap years
+  ---@type string
+  ---@diagnostic disable-next-line: assign-type-mismatch
   local start_year = os.date("%Y", start_time)
   local n_leap_days = 0
   if is_leap_year(start_year) then
+    ---@diagnostic disable-next-line: param-type-mismatch
     if start_time > date_to_time(os.date(start_year .. "/02/29", start_time)) then
       n_leap_days = (direction == "+") and 0 or 1
     else
@@ -236,22 +281,8 @@ local function handle_exact_date(spec)
 end
 
 --====================================================================================================
--- Command
+-- Commands
 --====================================================================================================
---- :Todo <ARG>...
---    - arg can be in the following forms
---      - "today"
---      - "mon[day]"|"tues[day]"|"wed[nedday]"|"thurs[day]"|"fri[day]"
---      - "<N>d"
---      - "<N>m"
---      - "<N>y"
---      - "(+|-)<N>d"
---      - "(+|-)<N>m"
---      - "(+|-)<N>y"
---      - "<Y><M><D>"
---      - "<Y>-<M>-<D>"
---      - "<Y>/<M>/<D>"
---    - if multiple args are supplied, they will be executed in sequence, one after another
 
 local handlers = {
   -- PERF: order by most common to least common
@@ -266,6 +297,61 @@ local handlers = {
   handle_relative_year,
 }
 
+---@param spec_list todo.Spec[] List of specs to handle sequentially
+---@return integer computed_time Resulting time after handling all specs, or -1 if aborted due to invalid spec
+local function process_specs(spec_list)
+  local time = get_start_time()
+
+  for _, spec in ipairs(spec_list) do
+    local handler_succeeded = false
+    for _, handler in ipairs(handlers) do
+      ---@diagnostic disable-next-line: redundant-parameter
+      local computed_time = handler(spec, time)
+      if computed_time then
+        time = computed_time
+        handler_succeeded = true
+        break
+      end
+    end
+
+    -- handle invalid spec
+    if not handler_succeeded then
+      if vim.g.todo_ignore_invalid_args then
+        vim.notify(
+          string.format("unable to parse argument '%s', ignoring and continuing", spec),
+          vim.log.levels.WARN,
+          { title = "TODO" }
+        )
+      else
+        vim.notify(
+          string.format("unable to parse argument '%s', aborting", spec),
+          vim.log.levels.ERROR,
+          { title = "TODO" }
+        )
+        return -1
+      end
+    end
+  end
+
+  return time
+end
+
+------------------------------------------------------------------------------------------------------
+--- :Todo <ARG>...
+--    - arg can be in any of the following forms
+--      - "today"
+--      - "mon[day]"|"tues[day]"|"wed[nedday]"|"thurs[day]"|"fri[day]"
+--      - "<N>d"
+--      - "<N>m"
+--      - "<N>y"
+--      - "(+|-)<N>d"
+--      - "(+|-)<N>m"
+--      - "(+|-)<N>y"
+--      - "<Y><M><D>"
+--      - "<Y>-<M>-<D>"
+--      - "<Y>/<M>/<D>"
+--    - if multiple args are supplied, they will be executed in sequence, one after another
+------------------------------------------------------------------------------------------------------
 vim.api.nvim_create_user_command("Todo", function(attrs)
   local nargs = #attrs.fargs
 
@@ -282,41 +368,11 @@ vim.api.nvim_create_user_command("Todo", function(attrs)
     return
   end
 
-  -- :Todo[!] <arg>...
+  -- :Todo <arg>...
   --------------------------------------------------
-  local start_buf = vim.api.nvim_win_get_buf(0)
-  local time = get_start_time()
-
-  -- handle each arg
-  for _, arg in ipairs(attrs.fargs) do
-    local handler_succeeded = false
-    for _, handler in ipairs(handlers) do
-      local computed_time = handler(arg, time)
-      if computed_time then
-        time = computed_time
-        handler_succeeded = true
-        break
-      end
-    end
-
-    -- handle invalid arg
-    if not handler_succeeded then
-      if vim.g.todo_ignore_invalid_args then
-        vim.notify(
-          string.format("unable to parse argument '%s', ignoring and continuing", arg),
-          vim.log.levels.WARN,
-          { title = "TODO" }
-        )
-      else
-        vim.notify(
-          string.format("unable to parse argument '%s', aborting", arg),
-          vim.log.levels.ERROR,
-          { title = "TODO" }
-        )
-        vim.api.nvim_win_set_buf(0, start_buf)
-        break
-      end
-    end
+  local time = process_specs(attrs.fargs)
+  if time == -1 then
+    return
   end
 
   -- navigate to computed date file
@@ -329,9 +385,63 @@ vim.api.nvim_create_user_command("Todo", function(attrs)
   end
 end, { nargs = "*" })
 
+------------------------------------------------------------------------------------------------------
+--- :TodoReschedule <ARG>...
+--    - same arguments as :Todo
+------------------------------------------------------------------------------------------------------
+vim.api.nvim_create_user_command("TodoReschedule", function(attrs)
+  if attrs.range == 0 then
+    return
+  end
+
+  local time = process_specs(attrs.fargs)
+  if time == -1 then
+    return
+  end
+
+  local date = os.date("%Y/%m/%d", time)
+  local date_file = string.format("%s/%s.norg", calendar_dir, date)
+  if not vim.uv.fs_stat(date_file) then
+    vim.notify(string.format("File '%s' does not exist", date_file), vim.log.levels.WARN, { title = "TODO" })
+    return
+  end
+
+  local items_to_reschedule = vim.api.nvim_buf_get_lines(0, attrs.line1 - 1, attrs.line2, true)
+
+  -- remove items from original file
+  local is_src_modified = vim.o.modified
+  vim.api.nvim_buf_set_lines(0, attrs.line1 - 1, attrs.line2, true, {})
+  if is_src_modified then
+    vim.notify(
+      "Source was in a modified state before rescheduling; not writing changes to disk",
+      vim.log.levels.WARN,
+      { title = "TODO" }
+    )
+  else
+    vim.cmd("w")
+  end
+
+  -- set items in target file
+  vim.cmd(string.format("edit %s", date_file))
+  local is_target_modified = vim.o.modified
+  local prev_last_line = vim.fn.line("$")
+  vim.api.nvim_buf_set_lines(0, -1, -1, true, items_to_reschedule)
+  if is_target_modified then
+    vim.notify(
+      "Target was in a modified state before rescheduling; not writing changes to disk",
+      vim.log.levels.WARN,
+      { title = "TODO" }
+    )
+  else
+    vim.cmd("w")
+  end
+  vim.cmd(string.format("normal! %sGjVG$", prev_last_line)) -- visually select rescheduled changes
+end, { nargs = "+", range = true })
+
 --====================================================================================================
--- Mapping
+-- Mappings
 --====================================================================================================
+
 vim.api.nvim_create_autocmd("BufRead", {
   callback = function()
     if string.match(vim.api.nvim_buf_get_name(0), todo_dir) then
@@ -340,6 +450,14 @@ vim.api.nvim_create_autocmd("BufRead", {
       end, { buffer = 0 })
       vim.keymap.set("n", ")", function()
         vim.cmd(string.format("Todo +%dd", vim.v.count1))
+      end, { buffer = 0 })
+      vim.keymap.set("x", "(", function()
+        vim.cmd("normal!" .. vim.api.nvim_replace_termcodes("<Esc>", true, true, true)) -- leave visual mode so '< and '> marks get set
+        vim.cmd(string.format("'<,'>TodoReschedule -%dd", vim.v.count1))
+      end, { buffer = 0 })
+      vim.keymap.set("x", ")", function()
+        vim.cmd("normal!" .. vim.api.nvim_replace_termcodes("<Esc>", true, true, true)) -- leave visual mode so '< and '> marks get set
+        vim.cmd(string.format("'<,'>TodoReschedule +%dd", vim.v.count1))
       end, { buffer = 0 })
     end
   end,
