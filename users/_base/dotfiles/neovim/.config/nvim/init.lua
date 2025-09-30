5175c43f56635b1e32fe1d4e18c798751ee01d64
Austin Liu <austinhliu@gmail.com>
2025-09-29 22:23:27 -0700

    feat(neovim): migrate to nvim-treesitter main branch

--- users/_base/dotfiles/neovim/.config/nvim/init.lua
+++ users/_base/dotfiles/neovim/.config/nvim/init.lua
@@ -1,7 +1,38 @@
+--------------------------------------------------------------------------------
+-- PROFILING
+--   - https://github.com/nvim-lua/plenary.nvim#plenaryprofile
+--------------------------------------------------------------------------------
+local is_recording = false
+local profiling_file = vim.uv.os_tmpdir() .. "/nvim_profile.log"
+local function toggle_profile()
+  if is_recording then
+    is_recording = false
+    vim.notify(
+      string.format(
+        "Logged profile to %s. You can view a flamegraph with 'inferno-flamegraph %s > flame.svg && firefox flame.svg'",
+        profiling_file,
+        profiling_file
+      )
+    )
+    require("plenary.profile").stop()
+  else
+    is_recording = true
+    vim.notify("Logging profile to " .. profiling_file)
+    require("plenary.profile").start(profiling_file, { flame = true })
+  end
+end
+vim.keymap.set("", "<f1>", toggle_profile)
+
+--------------------------------------------------------------------------------
+-- EXPERIMENTAL LUA MODULE LOADER
+--------------------------------------------------------------------------------
 if vim.loader then
   vim.loader.enable() -- builtin bytecode cache for loaded modules
 end
 
+--------------------------------------------------------------------------------
+-- DISABLE BUILTIN PLUGINS
+--------------------------------------------------------------------------------
 local builtin_plugins = {
   "gzip",
   "matchit",
@@ -17,6 +48,9 @@ for i = 1, #builtin_plugins do
   vim.g["loaded_" .. builtin_plugins[i]] = true
 end
 
+--------------------------------------------------------------------------------
+-- CUSTOM CONFIG
+--------------------------------------------------------------------------------
 require("config.autocmds")
 require("config.commands")
 require("config.functions")
--- users/_base/dotfiles/neovim/.config/nvim/lua/config/plugins/configurations/treesitter.lua
+++ users/_base/dotfiles/neovim/.config/nvim/lua/config/plugins/configurations/treesitter.lua
@@ -1,12 +1,47 @@
-require("nvim-treesitter.configs").setup({
-  auto_install = true,
-  ensure_installed = { "lua", "markdown", "markdown_inline", "yaml" },
-  sync_install = false,
-  highlight = {
-    enable = true,
-    additional_vim_regex_highlighting = false,
-  },
-  indent = {
-    enable = true,
-  },
+require("nvim-treesitter").setup({
+  -- Directory to install parsers and queries to
+  install_dir = vim.fn.stdpath("data") .. "/site",
+})
+
+--------------------------------------------------------------------------------
+-- AUTOMATE TREESITTER
+--   - automatically install parser for filetype if missing
+--   - automatically enable treesitter in filetypes with a treesitter parser
+--------------------------------------------------------------------------------
+local function ts_start(bufnr, parser_name)
+  vim.notify("Enabling treesitter parser for " .. parser_name, vim.log.levels.INFO, { title = "Treesitter" })
+  vim.treesitter.start(bufnr, parser_name)
+end
+
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  desc = "Enable treesitter",
+  callback = function(event)
+    local bufnr = event.buf
+    local filetype = event.match
+
+    if filetype == "" then
+      return
+    end
+
+    local parser_name = vim.treesitter.language.get_lang(filetype)
+    if not parser_name then
+      vim.notify(vim.inspect("No treesitter parser found for filetype: " .. filetype), vim.log.levels.WARN)
+      return
+    end
+
+    -- Parser not available through nvim-treesitter
+    if not vim.tbl_contains(require("nvim-treesitter").get_available(), parser_name) then
+      return
+    end
+
+    -- Check if parser is already installed
+    if not vim.tbl_contains(require("nvim-treesitter").get_installed("parsers"), parser_name) then
+      vim.notify("Installing treesitter parser for " .. parser_name, vim.log.levels.INFO)
+      require("nvim-treesitter").install({ parser_name }):await(function()
+        ts_start(bufnr, parser_name)
+      end)
+    else
+      ts_start(bufnr, parser_name)
+    end
+  end,
 })
