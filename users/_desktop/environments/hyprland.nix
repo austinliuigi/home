139e4230bc1f532e11f9e1952d84e1a61194d322
Austin Liu <austinhliu@gmail.com>
2026-03-04 09:52:14 -0800

    fix(hyprland): add gtk portal

--- users/_desktop/environments/hyprland.nix
+++ users/_desktop/environments/hyprland.nix
@@ -1,6 +1,23 @@
-{ config, pkgs, lib, ... }:
-
 {
+  config,
+  pkgs,
+  lib,
+  ...
+}: {
+  xdg.portal = {
+    enable = true;
+    extraPortals = [
+      pkgs.xdg-desktop-portal-hyprland
+      pkgs.xdg-desktop-portal-gtk
+    ];
+    config = {
+      hyprland = {
+        default = ["hyprland" "gtk"];
+        # "org.freedesktop.impl.portal.FileChooser" = "gtk";
+      };
+    };
+  };
+
   modules = {
     scripts = {
       battery.enable = true;
@@ -8,19 +25,20 @@
       volume.enable = true;
       wayland.screenshot.enable = true;
     };
+
     programs = {
-      wayland.hyprland.enable   = true;
-      wayland.pyprland.enable   = true;
-      wayland.hypridle.enable   = true;
-      wayland.hyprlock.enable   = true;
+      wayland.hyprland.enable = true;
+      wayland.pyprland.enable = true;
+      wayland.hypridle.enable = true;
+      wayland.hyprlock.enable = true;
       wayland.hyprpicker.enable = true;
-      wayland.swww.enable       = true;
-      wayland.swayidle.enable   = false;
-      wayland.fuzzel.enable     = true;
-      wayland.rofi.enable       = true;
-      wayland.waybar.enable     = true;
+      wayland.swww.enable = true;
+      wayland.swayidle.enable = false;
+      wayland.fuzzel.enable = true;
+      wayland.rofi.enable = true;
+      wayland.waybar.enable = true;
       wayland.wl-clipboard.enable = true;
-      wayland.wtype.enable      = true;
+      wayland.wtype.enable = true;
     };
   };
 }
