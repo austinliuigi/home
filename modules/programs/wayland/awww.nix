ff492e9b56c14e332c91b337ce937d69bb89e342
Austin Liu <austinhliu@gmail.com>
2026-08-07 00:45:35 -0700

    refactor: migrate pyprland config to new location and rename swww to awww

--- modules/programs/wayland/awww.nix
+++ modules/programs/wayland/awww.nix
@@ -1,28 +1,31 @@
-{ pkgs, lib, config, inputs, ... }:
-
-let
-  cfg = config.modules.programs.wayland.swww;
-in
 {
-  options.modules.programs.wayland.swww.enable = lib.mkEnableOption "swww module";
+  pkgs,
+  lib,
+  config,
+  inputs,
+  ...
+}: let
+  cfg = config.modules.programs.wayland.awww;
+in {
+  options.modules.programs.wayland.awww.enable = lib.mkEnableOption "awww module";
 
   config = lib.mkIf cfg.enable {
-    home.packages = [ pkgs.swww ];
+    home.packages = [pkgs.awww];
 
     modules.txn.wallpaper.enable = true;
 
     home.file = {
       ".cache/palette.dummy".onChange = lib.mkAfter ''
-        echo "swww: updating wallpaper"
+        echo "awww: updating wallpaper"
 
-        ${pkgs.swww}/bin/swww img ~/.cache/txn/wallpaper.png || true
+        ${pkgs.awww}/bin/awww img --transition-type center ~/.cache/txn/wallpaper.png || true
       '';
     };
     # home.activation = {
     #   wallpaper = lib.hm.dag.entryAfter ["writeBoundary"] ''
     #     if [ ! -d ~/wallpapers ]; then $DRY_RUN_CMD mkdir $VERBOSE_ARG ~/wallpapers; fi
     #     $DRY_RUN_CMD ${pkgs.imagemagick}/bin/convert ${config.dotfiles._wallpapers}/wallpaper.png -fill \#${config.colorscheme.colors.base02} -tint 100 ~/wallpapers/wallpaper.png 2> /dev/null
-    #     ${pkgs.swww}/bin/swww img ~/wallpapers/wallpaper.png
+    #     ${pkgs.awww}/bin/awww img ~/wallpapers/wallpaper.png
     #   '';
     # };
   };
--- modules/programs/wayland/pyprland.nix
+++ modules/programs/wayland/pyprland.nix
@@ -1,16 +1,19 @@
-{ pkgs, lib, config, inputs, ... }:
-
-let
-  cfg = config.modules.programs.wayland.pyprland;
-in
 {
+  pkgs,
+  lib,
+  config,
+  inputs,
+  ...
+}: let
+  cfg = config.modules.programs.wayland.pyprland;
+in {
   options.modules.programs.wayland.pyprland.enable = lib.mkEnableOption "pyprland module";
 
   config = lib.mkIf cfg.enable {
-    home.packages = [ pkgs.pyprland ];
+    home.packages = [pkgs.pyprland];
 
     home.file = {
-      ".config/hypr/pyprland.toml".source = config.lib.file.mkOutOfStoreSymlink "${config.dotfiles.pyprland}/.config/hypr/pyprland.toml";
+      ".config/pypr/config.toml".source = config.lib.file.mkOutOfStoreSymlink "${config.dotfiles.pyprland}/.config/pypr/config.toml";
     };
   };
 }
